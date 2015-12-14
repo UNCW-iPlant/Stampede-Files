8ad19793b8ec278f1f326f3f7f960efc9188329e
Talleyman <stephen522@earthlink.net>
2015-12-14 12:55:06 -0500

    Added application assets for version 0.9

--- Syngenta-Stuff/convert_syngenta_data.sh
+++
@@ -1,8 +0,0 @@
-#This script assumes you have the PhenoExtracter file 
-#and the fastlmmc executable in the folder with the syngenta datasets
-module load plink
-time while read line
-do plink --noweb --file $line --make-bed --out $line
-rm *.log *.nosex 
-python PhenoExtracter.py -f $line.fam
-done < syn_names.txt
